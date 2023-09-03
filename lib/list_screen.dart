8335ea27dce9241cb18ed1e36d02c562796d6e43
Eleasu-Hossain-Evan <eleaus16-364@diu.edu.bd>
2023-09-03 17:42:34 +0600

    rmv unused imports

--- lib/list_screen.dart
+++ lib/list_screen.dart
@@ -1,11 +1,6 @@
-import 'dart:developer';
-import 'dart:math';
-
-import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/detail_screen.dart';
 import 'package:flutter_app/provider.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shimmer/shimmer.dart';
 
